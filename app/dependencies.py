70d5b5e2896423b35c1c783d6c2d45c4ead2fc0c
Binaryy <ibolarinwa606@gmail.com>
2025-01-02 12:37:10 +0100

    corrected depends_get_user

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,9 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
+from models import User
+from sqlalchemy.orm import Session
+from app.routes import database
 
 # You should use the same secret key and algorithm you used for token creation in `auth.py`
 SECRET_KEY = "your_secret_key_here"
@@ -10,12 +13,15 @@ ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 # Function to get the current authenticated user
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-        return username
+        user = db.query(User).filter(User.username == username).first()
+        if user is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+        return user
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
